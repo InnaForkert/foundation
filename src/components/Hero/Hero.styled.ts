88259a927acee4609a2cf72dc20238e04dd21ed2
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-11 17:06:55 +0200

    feat: hero base markup

--- src/components/Hero/Hero.styled.ts
+++ src/components/Hero/Hero.styled.ts
@@ -1,1 +1,30 @@
-export const ExportPlaceholder = {};
+import styled from "styled-components";
+import heroImg from "../../assets/hero.jpg";
+
+export const StyledSection = styled.section`
+  min-height: 100vh;
+  padding: 1rem 3rem;
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+background-image: url(${hero}) ;
+  @media screen and (min-width: 768px) {
+    padding: 3rem 6rem;
+  }
+`;
+export const InfoWrapper = styled.div`
+  margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    max-width: 500px;
+    margin: 0;
+    margin-bottom: 32px;
+  }
+`;
+
+export const StyledMainTitle = styled.h1`
+ color: ${({ theme }) => theme.colors.dark};
+  font-weight: 700;
+  font-size: ${({ theme }) => theme.spacing(8)};
+`;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-
+import { StyledSection, InfoWrapper, StyledMainTitle} from "./Hero.styled";
 function Hero() {
-  return <section>
-    <h1>Благодійний фонд "НАЗВА ФОНДУ"</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum doloribus dolore impedit, nam, nostrum ab adipisci ducimus tempora placeat dignissimos vitae voluptatum suscipit perspiciatis reprehenderit ad a reiciendis quaerat aspernatur.
-    Iste nobis nulla veritatis, corrupti doloremque quia, reiciendis iure, provident impedit reprehenderit numquam! Autem quasi beatae et magni ullam eligendi, facilis mollitia animi aut soluta voluptatem consequatur! Sunt, natus nulla?
-    </p>
-    <h3>Основні завдання благодійного фонду:</h3>
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-  </section>;
+  return (
+    <StyledSection>
+      <InfoWrapper>
+        <StyledMainTitle>
+          БЛАГОДІЙНА ОРГАНІЗАЦІЯ "БЛАГОДІЙНИЙ ФОНД "ВОЛОНТЕРИ БАТЬКІВЩИНИ"
+        </StyledMainTitle>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+          doloribus dolore impedit, nam, nostrum ab adipisci ducimus tempora
+          placeat dignissimos vitae voluptatum suscipit perspiciatis
+          reprehenderit ad a reiciendis quaerat aspernatur. 
+        </p>
+        <h3>Основні завдання благодійного фонду:</h3>
+        <ul>
+          <li>допомога тимчасово переміщеним особам</li>
+          <li>допомога війську</li>
+          <li>психологічна підтримка тимчасово переміщеним особам</li>
+        </ul>
+        <a href="#">Детальніше</a>
+      </InfoWrapper>
+    </StyledSection>
+  );
 }
 
 export default Hero;
